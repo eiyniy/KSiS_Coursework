2cb3c17ee3b9a5ae5d21e9f683cbcaa3cdb14d6e
ilya.markevich <ilya.markevich.al@gmail.com>
2022-05-27 16:21:56 +0300

    add base message class and library

---
+++ Library/Messaging/Message.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Library.Messaging;
+
+public abstract class Message
+{
+    protected readonly byte[] _messageTypes = {
+        1,  //connect
+        2,  //disconnect
+    };
+
+
+    public byte MessageType { get; set; }
+
+
+    public abstract byte[] ToByteArray();
+}
